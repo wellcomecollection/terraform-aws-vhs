f62e0544687a7361810a14ef78a5e198cfc5d365
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 12:28:48 +0000

    update a few stragling name changes

--- examples/hash-range-store/main.tf
+++ examples/hash-range-store/main.tf
@@ -1,6 +1,6 @@
 module "example_implementation" {
-  source = "..\/..\/hash-store"
-  name = "multi_version_store"
+  source = "../../hash-range-store"
+  name = "hash_range_store"
   table_name_prefix = "org-vhs"
   bucket_name_prefix = "org-vhs"
 }
--- examples/hash-store/main.tf
+++ examples/hash-store/main.tf
@@ -1,6 +1,6 @@
 module "example_implementation" {
-  source = "..\/..\/hash-range-store"
-  name = "single_version_store"
+  source = "../../hash-store"
+  name = "hash_store"
   table_name_prefix = "org-vhs"
   bucket_name_prefix = "org-vhs"
 }
