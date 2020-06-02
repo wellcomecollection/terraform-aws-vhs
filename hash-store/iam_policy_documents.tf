c2a3d88ba1f29567c19f1dfe686c647d7ea73877
Alex Chan <alex@alexwlchan.net>
2020-06-02 07:33:02 +0100

    Reduce the S3 permissions given to the VHS

--- hash-store/iam_policy_documents.tf
+++ hash-store/iam_policy_documents.tf
@@ -44,7 +44,9 @@ data "aws_iam_policy_document" "full_access_policy" {
 
   statement {
     actions = [
-      "s3:*",
+      "s3:List*",
+      "s3:List*",
+      "s3:PutObject*",
     ]
 
     resources = [
