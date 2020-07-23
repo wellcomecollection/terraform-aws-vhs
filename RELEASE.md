5336dbb8ed9bb4fe81a100d581020b41a9f9f3ef
Alex Chan <alex@alexwlchan.net>
2020-07-23 08:07:28 +0100

    Cycle S3 objects to Standard IA after 30 days

---
+++ RELEASE.md
@@ -0,0 +1,6 @@
+RELEASE_TYPE: minor
+
+Automatically cycle S3 objects in VHS to the Standard-IA storage class after 30 days.
+This reduces the cost of running a VHS.
+
+You can disable this behaviour by setting `cycle_objects_to_standard_ia = false`.
--- hash-range-store/s3.tf
+++ hash-range-store/s3.tf
@@ -2,4 +2,14 @@ resource "aws_s3_bucket" "bucket" {
   bucket = local.bucket_name
 
   tags = var.tags
+
+  lifecycle_rule {
+    id      = "objects_to_standard_ia"
+    enabled = var.cycle_objects_to_standard_ia
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
 }
