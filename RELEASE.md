db10202a98f726c6f43d016ab2b08c53f03b5d31
Alex Chan <a.chan@wellcome.org>
2021-06-23 15:38:38 +0100

    Continue trying to fix the `assumable_read_role` output

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: patch
+
+Continue trying to fix the `assumable_read_role` output.
--- single-version-store/outputs.tf
+++ single-version-store/outputs.tf
@@ -11,7 +11,7 @@ output "full_access_policy" {
 }
 
 output "assumable_read_role" {
-  value = length(var.read_principals) > 0 ? aws_iam_role.assumable_read_role.arn : ""
+  value = length(var.read_principals) > 0 ? aws_iam_role.assumable_read_role.arn[0] : ""
 }
 
 output "table_name" {
